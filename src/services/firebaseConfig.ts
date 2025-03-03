09610dde98d18dd092e501eed372be74632bd910
Woker21 <112359676+woker21@users.noreply.github.com>
2025-03-03 14:54:45 +0100

    firebaseconfig

---
+++ src/services/firebaseConfig.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCv8YGMC7IESRf8VO2-AGBDakP7W9sBeTs",
+  authDomain: "red-button-firebase.firebaseapp.com",
+  projectId: "red-button-firebase",
+  storageBucket: "red-button-firebase.firebasestorage.app",
+  messagingSenderId: "18589363559",
+  appId: "1:18589363559:web:07b416aee89ce4b99b53c2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
