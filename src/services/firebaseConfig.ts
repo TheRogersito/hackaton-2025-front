9fb34d131202d8cf24109d0dbe971decf39b7245
TheRogersito <64012354+TheRogersito@users.noreply.github.com>
2025-03-03 14:06:36 +0000

    firebase config done

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,4 +1,10 @@
 import { initializeApp } from "firebase/app";
+import { getAuth, setPersistence, browserLocalPersistence } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+export { collection, doc, addDoc, getDoc, setDoc, getDocs, query, updateDoc, deleteDoc, where, onSnapshot, documentId } from "firebase/firestore";
+export { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail, fetchSignInMethodsForEmail, sendEmailVerification } from "firebase/auth";
+export { uploadBytes, getDownloadURL, ref } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCv8YGMC7IESRf8VO2-AGBDakP7W9sBeTs",
@@ -10,4 +16,9 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export const auth = getAuth(app);
+setPersistence(auth, browserLocalPersistence);
