2bfc89d78fd82f5d1a36428d11ae0a8a06f28ae0
Woker21 <112359676+woker21@users.noreply.github.com>
2025-03-04 11:26:48 +0100

    build in progress

--- src/app/Register/page.tsx
+++ src/app/Register/page.tsx
@@ -3,10 +3,7 @@ import { useRouter } from "next/navigation";
 import { signInWithGoogle } from "../../services/firebaseConfig";
 import { useUserStore } from "@/store/userStore";
 import { FcGoogle } from "react-icons/fc";
-
-
 import { kyc } from "@/services/kyc";
-
 
 export default function Register() {
     const router = useRouter();
@@ -17,35 +14,37 @@ export default function Register() {
             const user = await signInWithGoogle();
 
             if (user) {
-            const kycPayload = {
-                phoneNumber: "+34646519770", // Placeholder, should get from user input
-                idDocument: "FIC80142", // Should be collected from the user
-                name: user.displayName || "Unknown",
-                givenName: user.displayName?.split(" ")[0] || "Unknown",
-                familyName: user.displayName?.split(" ").slice(1).join(" ") || "Unknown",
-                address: "Street example", // Should be collected from user
-                streetName: "Example",
-                streetNumber: "1",
-                postalCode: "00000",
-                region: "Region",
-                locality: "City",
-                country: "ES",
-                birthdate: "2000-01-01", // Should be collected
-                email: user.email || "",
-                gender: "FEMALE", // Should be collected
-            };
+                const kycPayload = {
+                    phoneNumber: "+34646519770", // Placeholder, should get from user input
+                    idDocument: "FIC80142", // Should be collected from user
+                    name: user.displayName || "Unknown",
+                    givenName: user.displayName?.split(" ")[0] || "Unknown",
+                    familyName: user.displayName?.split(" ").slice(1).join(" ") || "Unknown",
+                    address: "Street example", // Should be collected from user
+                    streetName: "Example",
+                    streetNumber: "1",
+                    postalCode: "00000",
+                    region: "Region",
+                    locality: "City",
+                    country: "ES",
+                    birthdate: "2000-01-01", // Should be collected
+                    email: user.email || "",
+                    gender: "FEMALE", // Should be collected
+                };
 
-            // Call KYC API
-            const kycReady = await kyc(kycPayload);
-            if(kycReady.idDocumentMatch){
+                // Llamada a la API de KYC
+                const kycReady = await kyc(kycPayload);
 
+                // Comprobar si la propiedad idDocumentMatch es verdadera
+                if (kycReady.idDocumentMatch) {
                     setUser({
                         uid: user.uid,
-                        tutorName: user.displayName,
-                        email: user.email,
-                        photoURL: user.photoURL,
-                        role: "sender", 
-                    })
+                        tutorName: user.displayName ?? undefined,  // Usamos undefined en lugar de null
+                        email: user.email ?? undefined,  // Usamos undefined en lugar de null
+                        photoURL: user.photoURL ?? undefined,  // Usamos undefined en lugar de null
+                        role: "sender",
+                    });
+
                     router.push("/RegisterData");
                 }
             }
